3f903bc190614c4ed603c067d3f4f9905b87d881
romanb123 <44100462+romanb123@users.noreply.github.com>
2020-08-30 15:35:41 +0300

    edit-3

--- index.php
+++ index.php
@@ -1,33 +1,109 @@
 <?php
 get_header();
 ?>
-<div class="main-content">
-    <div class="heading">
+<div class='main-content'>
+    <div class='heading'>
         <h1>
             Roman
         </h1>
         <h2>
             web development
         </h2>
-        <div class="btn">
+        <div class='btn'>
             <button>contact us</button>
         </div>
     </div>
 </div>
-    <h3>servises</h3>
-<div class="servises">
-    <div class="servise-item">
-        בניית תארים
+<!-- services -->
+<h3 class='servises-heading'>servises</h3>
+<div class='servises'>
+    <div class='servise-item'>
+        <img src='http://localhost/e-shop/wp-content/uploads/2020/08/html-five.png'>
+        <h3>בניית אתרים</h3>
+        <p>Lorem Ipsum is simply dummy text of the printing
+            and typesetting industry. Lorem Ipsum has been
+            the industry's standard dummy text ever since the
+            1500s, when an unknown printer took a galley of type and scrambled
+            it to make a type specimen book. It has survived not only five centuries,</p>
     </div>
     <div class="servise-item">
-        בניית דפי נחיתה
+        <img src="http://localhost/e-shop/wp-content/uploads/2020/08/html-five2.png">
+        <h3>בניית דפי נחיתב</h3>
+        <p>Lorem Ipsum is simply dummy text of the printing
+            and typesetting industry. Lorem Ipsum has been
+            the industry's standard dummy text ever since the
+            1500s, when an unknown printer took a galley of type and scrambled
+            it to make a type specimen book. It has survived not only five centuries, </p>
+    </div>
+    <div class='servise-item'>
+        <img src='http://localhost/e-shop/wp-content/uploads/2020/08/wordpress.png'>
+        <h3>עיצוב אתרים</h3>
+        <p>Lorem Ipsum is simply dummy text of the printing
+            and typesetting industry. Lorem Ipsum has been
+            the industry's standard dummy text ever since the
+            1500s, when an unknown printer took a galley of type and scrambled
+            it to make a type specimen book. It has survived not only five centuries,</p>
     </div>
     <div class="servise-item">
-        עיצוב אתרים
+        <img src="http://localhost/e-shop/wp-content/uploads/2020/08/android.png">
+        <h3>בתאמב למובייל</h3>
+        <p>Lorem Ipsum is simply dummy text of the printing
+            and typesetting industry. Lorem Ipsum has been
+            the industry's standard dummy text ever since the
+            1500s, when an unknown printer took a galley of type and scrambled
+            it to make a type specimen book. It has survived not only five centuries, </p>
     </div>
-    <div class="servise-item">
-        בתאמה למובייל
+</div>
+<!-- projects -->
+<div class='projects-section'>
+    <h2>projects</h2>
+    <div class='projects-layout'>
+        <div class='project-item'>
+            <img src='http://localhost/e-shop/wp-content/uploads/2020/08/pr_img2-scaled.jpg' width='230' height='200'>
+            <ul>
+                <h2>e shop project</h2>
+                <li>angular</li>
+                <li>css 3</li>
+                <li>node js</li>
+            </ul>
+            <button class='project-btn'>project link</button>
+        </div>
+        <div class='project-item'>
+            <img src='http://localhost/e-shop/wp-content/uploads/2020/08/pr_img1-scaled.jpg' width='230' height='200'>
+            <ul>
+                <h2>e shop project</h2>
+                <li>angular</li>
+                <li>css 3</li>
+                <li>node js</li>
+            </ul>
+            <button class='project-btn'>project link</button>
+        </div>
+        <div class='project-item'>
+            <img src='http://localhost/e-shop/wp-content/uploads/2020/08/pr_img4-scaled.jpg' width='230' height='200'>
+            <ul>
+                <h2>e shop project</h2>
+                <li>angular</li>
+                <li>css 3</li>
+                <li>node js</li>
+            </ul>
+            <button class='project-btn'>project link</button>
+
+        </div>
+        <div class='project-item'>
+            <img src='http://localhost/e-shop/wp-content/uploads/2020/08/pr_img3-scaled.jpg' width='230' height='200'>
+            <ul>
+                <h2>e shop project</h2>
+                <li>angular</li>
+                <li>css 3</li>
+                <li>node js</li>
+            </ul>
+            <button class='project-btn'>project link</button>
+
+        </div>
     </div>
+</div>
+<div class="contact-us">
+
 </div>
 <?php
 get_footer();
